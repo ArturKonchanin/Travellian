750e8e79cdb533b999853711b4350c263dc5af45
AKonchanin <konchanin.artur@gmail.com>
2022-12-14 22:08:18 +0300

    update section & js

--- js/box-slider.js
+++ js/box-slider.js
@@ -36,7 +36,7 @@ if (animItems.length > 0) {
 	}, 300);
 }
 */
-
+/*
 $(document).ready(function(){
 	$('.slider ul').bxSlider({
 		controls: true,
@@ -57,6 +57,113 @@ $(document).ready(function(){
 		slideWidth: 394      
 	});
 });
+*/
+/*
+
+ var windowWidth = $(window).width();  
+	var obxSettings = {
+		controls: true,
+		prevSelector: '.slider-title-prev',
+		nextSelector: '.slider-title-next',        
+		pager: false,		
+		auto: false,
+		pause: 0,
+		infiniteLoop: true,
+		adaptiveHeight: true,
+		responsive: true,
+		nextText: '',
+		prevText: '',
+		moveSlides: 1,
+		minSlides: 3,
+		maxSlides: 3,
+		slideMargin: 32,  
+		slideWidth: 394
+	};
+ $('.slider ul').bxSlider(obxSettings);
+
+ $(window).resize(function() {
+    windowWidth = $(window).width();
+	 if (windowWidth < 1000) {
+		 console.log(obxSettings.minSlides);
+		obxSettings.minSlides = 2;  
+		obxSettings.maxSlides = 2;
+		obxSettings.slideMargin = 60;  
+		obxSettings.slideWidth = 200;
+	 } else {
+		 console.log(windowWidth);
+		obxSettings.minSlides = 3;  
+		obxSettings.maxSlides = 3;
+		 
+ }
+
+ });
+*/
+ // Use the conventional $ prefix for variables that hold jQuery objects.
+var $slider;
+
+// If the only purpose of the windowWidth() function is to set the slide variables,
+// it can be renamed and rewritten to supply the full configuration object instead.
+function buildSliderConfiguration() {
+
+    // When possible, you should cache calls to jQuery functions to improve performance.
+    var windowWidth = $(window).width();
+    var numberOfVisibleSlides;
+
+    if (windowWidth < 420) {
+        numberOfVisibleSlides = 1;
+    } else if (windowWidth < 768) {
+        numberOfVisibleSlides = 2;
+    } else  {
+        numberOfVisibleSlides = 3;
+    } 
+
+    return {
+        pager: false,
+		controls: true,
+		prevSelector: '.slider-title-prev',
+		nextSelector: '.slider-title-next',
+        auto: false,
+        slideWidth: 5000,
+        startSlide: 0,
+        nextText: ' ',
+        prevText: ' ',
+        adaptiveHeight: true,
+        moveSlides: 1,
+        slideMargin: 20,
+        minSlides: numberOfVisibleSlides,
+        maxSlides: numberOfVisibleSlides
+    };
+}
+
+// This function can be called either to initialize the slider for the first time
+// or to reload the slider when its size changes.
+function configureSlider() {
+    var config = buildSliderConfiguration();
+
+    if ($slider && $slider.reloadSlider) {
+        // If the slider has already been initialized, reload it.
+        $slider.reloadSlider(config);
+    } else {
+        // Otherwise, initialize the slider.
+        $slider = $('.slider ul').bxSlider(config);
+    }
+}
+
+$('.slider-prev').click(function () {
+    var current = $slider.getCurrentSlide();
+    $slider.goToPrevSlide(current) - 1;
+});
+
+$('.slider-next').click(function () {
+    var current = $slider.getCurrentSlide();
+    $slider.goToNextSlide(current) + 1;
+});
+
+// Configure the slider every time its size changes.
+$(window).on("orientationchange resize", configureSlider);
+// Configure the slider once on page load.
+configureSlider();
+
 
 $(document).ready(function(){
 	$('.slider2 ul').bxSlider({
@@ -78,6 +185,8 @@ $(document).ready(function(){
 		slideWidth: 394      
 	});
 });
+
+
 
 $(document).ready(function(){
 	$('.slider3 ul').bxSlider({
